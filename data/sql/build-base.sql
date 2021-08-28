74bd28e40e6dfc9e93cb4bfb38e7b4b5b175270f
joaohudson <joaoliveira@cc.ci.ufpb.br>
2021-08-28 03:20:52 -0300

    fixed database build

--- data/sql/build-base.sql
+++ data/sql/build-base.sql
@@ -1,3 +1,19 @@
 CREATE DATABASE crud;
 
-CREATE TABLE User(id not null auto_increment, name varchar(100), gender varchar(6), age int, login varchar(100), password varchar(100));
+USE crud;
+
+CREATE TABLE User(
+	id int not null auto_increment,
+	name varchar(100),
+    gender varchar(6),
+    age int,
+    login varchar(100),
+    password varchar(100),
+    primary key(id));
+    
+CREATE TABLE Conta(
+	id int not null auto_increment,
+    balance decimal,
+    id_User int,
+    primary key(id),
+    foreign key(id_User) references User(id));
