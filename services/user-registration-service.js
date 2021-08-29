8731e9a75edf944e17f3213ae4dcd0aa11e4b938
joaohudson <joaoliveira@cc.ci.ufpb.br>
2021-08-28 23:03:38 -0300

    Adicionando serviço de registro de usuários

---
+++ services/user-registration-service.js
@@ -0,0 +1,24 @@
+import userRepository from "./user-repository.js"
+import contaRepository from "./conta-repository.js"
+import User from "../models/user.js"
+import Conta from "../models/conta.js";
+
+const registre = async (data) => {
+    try{
+        const user = new User(data);
+        const conta = new Conta();
+
+        await userRepository.add(user);
+
+        const domainUser = await userRepository.getByLogin(user.login);
+        
+        await contaRepository.add(conta, domainUser.id);
+    }
+    catch(e){
+        throw e;
+    }
+}
+
+export default {
+    registre
+}
