c0bfa52517fa472658b785aa32ecce6ab69a435a
joaohudson <joaoliveira@cc.ci.ufpb.br>
2021-08-28 23:25:35 -0300

    Adicionando controller de conta

---
+++ controllers/conta.js
@@ -0,0 +1,20 @@
+import contaRepository from "../services/conta-repository.js"
+
+const find = async (req, res) => {
+    try{
+        const userId = req.query.userId;
+        
+        if(!userId){
+            throw 'Id do usuário não informado!';
+        }
+
+        const conta = await contaRepository.findByUserId(userId);
+        res.json({ok: true, data: conta});
+    }catch(e){
+        res.json({ok: false, message: e});
+    }
+}
+
+export default {
+    find
+}
